96c8f3a285c17ff41c296a23669fcf27cf2f8cf1
Ryan Crystian <164635303+Ryan77y@users.noreply.github.com>
2024-11-25 17:54:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Elden Ring',
+    'Quem é o chefe final do jogo?',
+    'Radagon e a Besta Pristina'
+)
+
+criaCartao(
+    'RDR2',
+    'Quem foi o traidor da gangue?',
+    'Micah Bell'
+)
+
+criaCartao(
+    'GTA VI',
+    'Em qual local GTA VI é inspirado?',
+    'Miami'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual o jogo mais vendido de todos os tempos?',
+    'Tetris 520 milhões de cópias'
+)
